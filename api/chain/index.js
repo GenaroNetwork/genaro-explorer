459f7e552f8065c1ea899c0f90b48fc6d31108b1
lishi <lishi1608@gmail.com>
2018-08-31 17:03:07 +0800

    add receipt data
    Signed-off-by: lishi <lishi1608@gmail.com>

--- api/chain/index.js
+++ api/chain/index.js
@@ -1,23 +1,28 @@
 const db = require('../db')
 const getWeb3 = require('./web3Manager')
 
-function addBlock(block) {
-    db.addBlock(block)
-}
-
 async function sync() {
     console.log('start sync: ' + Date.now())
     const web3 = getWeb3()
     const latestBlockHave = db.getStat().latestBlock
-    const latestBlockReal = await web3.eth.getBlockNumber()
+    const latestBlockReal = 100 //await web3.eth.getBlockNumber()
     for(let i = latestBlockHave + 1; i <= latestBlockReal; i ++) {
-        const block = await web3.eth.getBlock(i, true);
+        const block = await web3.eth.getBlock(i, true)
+        const promiReceipt = block.transactions.map(txHash => web3.eth.getTransactionReceipt(txHash))
+        const receipts = await Promise.all(promiReceipt)
+        // merge receipt
+        for(let j = 0; j < block.transactions.length; j ++) {
+            Object.assign(block.transactions[j], receipts[j]);
+        }
+        console.log(block.transactions)
         console.log('add block: ' + block.number + ' ' + Date.now())
-        addBlock(block)
+        db.addBlock(block, receipts)
         console.log('end add block ' + Date.now())
     }
     console.log('sync finish' + Date.now())
 }
+
+// function getPending
 
 module.exports = {
     sync
