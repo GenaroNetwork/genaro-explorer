aa7cecda02204067280ca9abc1e1ef8486c0deaf
chromer <chuliang2013@gmail.com>
2018-09-13 18:25:32 +0800

    fix miner address format

--- ui/src/views/Blocks.vue
+++ ui/src/views/Blocks.vue
@@ -93,7 +93,7 @@ export default {
               props: {
                 to: `/accounts/${params.row.miner}`
               },
-            }, params.row.miner)
+            }, params.row.miner.toLowerCase())
           }
         },
         {
